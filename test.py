8b0c41666b02d9130c94f2115db7c777fd22b857
MaskedGod <codegobbledygook@gmail.com>
2024-02-08 20:52:54 +0400

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+  # if event.type == pygame.MOUSEMOTION:
+     #     print(event.pos)    
+
+# mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint((mouse_pos)):
+    #     print(pygame.mouse.get_pressed())
+
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #     print("jump")
