23fe67677e2933f6fec9887672103bf6239cf11c
MaskedGod <codegobbledygook@gmail.com>
2024-02-10 12:50:45 +0400

    better enemy spawn logic

--- runner.py
+++ runner.py
@@ -14,11 +14,24 @@ def obstacle_movement(obstacle_list):
     if obstacle_list:
         for obstacle_rect in obstacle_list:
             obstacle_rect.x -= 5
-            screen.blit(snail_surf,obstacle_rect)
+            
+            if obstacle_rect.bottom == 300:
+                screen.blit(snail_surf,obstacle_rect)
+            else:
+                screen.blit(fly_surf,obstacle_rect)
         
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+
         return obstacle_list
     else: 
         return []
+
+def collisions(player,obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect):
+                return False
+    return True
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
@@ -39,7 +52,8 @@ ground_surf = pygame.image.load('graphics/ground.png').convert()
 
 # Obstacles
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surf.get_rect(bottomright=(600,300))
+
+fly_surf = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
 
 obstacle_rect_list = []
 
@@ -79,12 +93,13 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                snail_rect.left = 800
                 start_time = int((pygame.time.get_ticks()/1000))
 
         if event.type == obstacle_timer and game_active:
-            obstacle_rect_list.append(snail_surf.get_rect(bottomright=(randint(900,1100),300)))
-
+            if randint(0,2):
+                obstacle_rect_list.append(snail_surf.get_rect(bottomright=(randint(900,1100),300)))
+            else:
+                obstacle_rect_list.append(fly_surf.get_rect(bottomright=(randint(900,1100),210)))
 
     #Game
     if game_active:
@@ -110,12 +125,15 @@ while True:
         # Obstacle movement
         obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
-        # collision
-        if snail_rect.colliderect(player_rect):
-            game_active = False
+        # COllisions
+        game_active = collisions(player_rect,obstacle_rect_list)
+
     else:
         screen.fill((94,129,162))
         screen.blit(player_stand,player_stand_rect)
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80,300)
+        player_gravity = 0
 
         score_message = test_font.render(f"Your score: {score}",False,(111,196,169))
         score_message_rect = score_message.get_rect(center=(400,330))
